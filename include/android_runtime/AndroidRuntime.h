c6b457d2b116bfa02fa39389eac4571ba529276b
Kenan Garibov <kengnatural@gmail.com>
2016-02-06 17:19:51 +0200

    AndroidRuntime legacy startvm() function returned back to fix broken xposed framework. Fix proposed by Stefan Berger <s.berger81@gmail.com>.
    Change-Id: I5fad896519b2c58086be534a67b69989080712d3

--- include/android_runtime/AndroidRuntime.h
+++ include/android_runtime/AndroidRuntime.h
@@ -64,6 +64,7 @@ public:
 
     int addVmArguments(int argc, const char* const argv[]);
 
+    void start(const char *classname, const char* options);
     void start(const char *classname, const char* options, bool zygote);
 
     void exit(int code);
@@ -117,6 +118,7 @@ private:
     static int startReg(JNIEnv* env);
     void parseExtraOpts(char* extraOptsBuf);
     int startVm(JavaVM** pJavaVM, JNIEnv** pEnv, bool zygote);
+    int startVm(JavaVM** pJavaVM, JNIEnv** pEnv);
 
     Vector<JavaVMOption> mOptions;
     bool mExitWithoutCleanup;
